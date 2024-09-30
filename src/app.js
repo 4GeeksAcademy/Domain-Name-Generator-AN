37ece513bf71a466f0ecd60730800f094c4df3b3
Sajadev404 <empresasarn@gmail.com>
2024-09-30 18:07:04 +0000

    firts commit

--- src/app.js
+++ src/app.js
@@ -8,14 +8,10 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let a = 0; a < ext.length; a++){
-        //en caso que el dominio acabe similar a alguna extension añadirla
-       let dominio = pronoun[i] + adj[j] + noun[k];
+       
+       let dominio = pronoun[i] + adj[j] + noun[k] + ext[a];
        console.log(dominio);
-       if(noun[k] == ext[a]){
-        let hack = noun[k].slice(0, noun[k].length - 1) + ext[a];
-        console.log(hack);
        }
-    }
   }
 }
 }
