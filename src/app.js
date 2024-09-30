48315fa6cdc1920fb54cea4f8e579d9bf2470fd6
Sajadev404 <empresasarn@gmail.com>
2024-09-30 18:00:27 +0000

    firts commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "his"];
+let adj = ["great", "big", "amazing"];
+let noun = ["jogger", "racoon", "dogcom", "merchant", "driverus","es"];
+let ext = [".com",".net",".us",".es"];
+// generating the domains
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let a = 0; a < ext.length; a++){
+        //en caso que el dominio acabe similar a alguna extension añadirla
+       let dominio = pronoun[i] + adj[j] + noun[k];
+       console.log(dominio);
+       if(noun[k] == ext[a]){
+        let hack = noun[k].slice(0, noun[k].length - 1) + ext[a];
+        console.log(hack);
+       }
+    }
+  }
+}
+}
